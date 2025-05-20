a2c3b7335d99612082343ca02b24a413b013a02e
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2025-05-20 20:14:39 +0300

    Update maintainer

--- app/probe.py
+++ app/probe.py
@@ -19,7 +19,7 @@ re_flags = re.I | re.U | re.X
 class Check(object):
     def __init__(self, *a, **kw):
         self._program = "ovhbackup_lag_checks"
-        self._author = "Mathieu Le Marec - Pasquet (kiorky)"
+        self._author = "Makina Corpus (kiorky)"
         self._nick = self._program.replace("check_", "")
         self._ok = 0
         self._warning = 1
